cf4d7fd644c08278e0b4c16d63a62820ff571a61
Esther <moutain60237@gmail.com>
2020-08-02 22:49:58 +0800

    feat: add findUser function

---
+++ findUser.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+function findUser(userInfo) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === userInfo.email && users[i].password === userInfo.password) {
+      return users[i]
+    }
+  }
+  return false
+}
+
+module.exports = findUser
--- routes/modules/login.js
+++ routes/modules/login.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+const findUser = require('../../findUser')
 
 router.get('/', (req, res) => {
   res.render('index')
 })
+
 router.post('/', (req, res) => {
-  res.render('welecome')
+  const userInfo = req.body
+  const result = findUser(userInfo)
+  console.log(result)
+  if (result) {
+    res.render('welecome', { result })
+  }
+  else {
+    res.render('index')
+  }
 })
-
-
-
 module.exports = router
